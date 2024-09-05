c237423ec5c585ec5022f14028cb6ccdc4bf6a4e
Luca Pasquale <lucapasquale.094@gmail.com>
2024-09-05 13:07:54 -0300

    upgraded deps

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(), TanStackRouterVite()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 });
