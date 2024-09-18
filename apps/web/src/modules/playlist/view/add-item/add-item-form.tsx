f645812dc1190bc0e229dc3307f3e5cd36baa430
Luca Pasquale <lucapasquale.094@gmail.com>
2024-09-18 16:06:42 -0300

    Don't show add video form error if not touched

--- apps/web/src/modules/playlist/view/add-item/add-item-form.tsx
+++ apps/web/src/modules/playlist/view/add-item/add-item-form.tsx
@@ -42,6 +42,10 @@ export function AddItemForm({ onAdd }: Props) {
   });
 
   const onBlur = async () => {
+    if (!form.formState.isDirty) {
+      return;
+    }
+
     const [valid, response] = await Promise.all([
       form.trigger("url"),
       getUrlInfo({ variables: { url: form.getValues("url") } }),
