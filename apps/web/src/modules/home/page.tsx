4d1ee1c47ab7a5697fceae893c0f31232217f832
Luca Pasquale <lucapasquale.094@gmail.com>
2024-09-30 18:32:19 -0300

    checkpoint

--- apps/web/src/modules/home/page.tsx
+++ apps/web/src/modules/home/page.tsx
@@ -1,11 +1,5 @@
 import { useQuery } from "@apollo/client";
-import { zodResolver } from "@hookform/resolvers/zod";
 import { Link } from "@tanstack/react-router";
-import { SliderFormItem } from "@ui/components/form/slider-form-item";
-import { Button } from "@ui/components/ui/button";
-import { Form } from "@ui/components/ui/form";
-import { useForm } from "react-hook-form";
-import { z } from "zod";
 
 import { HomePlaylistsDocument } from "~common/graphql-types";
 
@@ -30,50 +24,7 @@ export function Page() {
             </li>
           ))}
         </ol>
-
-        <div className="container max-w-[500px]">
-          <TestForm />
-        </div>
       </section>
     </main>
-  );
-}
-
-const schema = z.object({
-  slider: z.tuple([z.number(), z.number()]),
-});
-type FormValues = z.infer<typeof schema>;
-
-function TestForm() {
-  const form = useForm<FormValues>({
-    resolver: zodResolver(schema),
-    defaultValues: { slider: [0, 100] },
-  });
-
-  const onSubmit = async (values: FormValues) => {
-    console.log(values);
-  };
-
-  return (
-    <Form {...form}>
-      <form
-        id="test"
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="w-full flex flex-col items-center gap-4"
-      >
-        <SliderFormItem
-          control={form.control}
-          name="slider"
-          label="Slider"
-          min={0}
-          max={100}
-          marks={[9, 15, 25, 50, 78]}
-        />
-
-        <Button type="submit" form="test" disabled={!form.formState.isValid}>
-          Submit
-        </Button>
-      </form>
-    </Form>
   );
 }
