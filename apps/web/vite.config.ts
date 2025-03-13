25080b37be513142884f9f1e84ae9b1b2a20bd4d
Luca Pasquale <lucapasquale.094@gmail.com>
2025-03-13 14:48:10 -0300

    Reverted to tailwind v3

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths(), tailwindcss(), TanStackRouterVite()],
+  plugins: [react(), tsconfigPaths(), TanStackRouterVite()],
 });
