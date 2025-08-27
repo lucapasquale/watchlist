0d798ef99bc670454959e335aa221fbd8537c534
Luca Pasquale <lucapasquale.094@gmail.com>
2025-08-26 22:45:42 -0300

    using vite-rolldown

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,10 +1,10 @@
 import tailwindcss from "@tailwindcss/vite";
-import { TanStackRouterVite } from "@tanstack/router-vite-plugin";
-import react from "@vitejs/plugin-react-swc";
+import { tanstackRouter } from "@tanstack/router-vite-plugin";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import svgr from "vite-plugin-svgr";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), svgr(), tailwindcss(), tsconfigPaths(), TanStackRouterVite()],
+  plugins: [react(), svgr(), tailwindcss(), tsconfigPaths(), tanstackRouter()],
 });
